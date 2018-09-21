03ad637fb10a139b97d90ac73690db81942a3919
Vanessa Dannenberg <vanessa.e.dannenberg@gmail.com>
2018-09-21 17:57:42 -0400

    add 5, 10, 25 cent coins, and 100 Mg note

--- craftitems.lua
+++ craftitems.lua
@@ -2,6 +2,27 @@
 local MP = minetest.get_modpath(minetest.get_current_modname())
 local S, NS = dofile(MP.."/intllib.lua")
 
+minetest.register_craftitem("currency:minegeld_cent_5", {
+    description = S("5 Minegeld cent coin"),
+    inventory_image = "minegeld_cent_5.png",
+        stack_max = 30000,
+		groups = {minegeld = 1}
+})
+
+minetest.register_craftitem("currency:minegeld_cent_10", {
+    description = S("10 Minegeld cent coin"),
+    inventory_image = "minegeld_cent_10.png",
+        stack_max = 30000,
+		groups = {minegeld = 1}
+})
+
+minetest.register_craftitem("currency:minegeld_cent_25", {
+    description = S("25 Minegeld cent coin"),
+    inventory_image = "minegeld_cent_25.png",
+        stack_max = 30000,
+		groups = {minegeld = 1}
+})
+
 minetest.register_craftitem("currency:minegeld", {
     description = S("1 Minegeld Note"),
     inventory_image = "minegeld.png",
@@ -26,6 +47,13 @@ minetest.register_craftitem("currency:minegeld_10", {
 minetest.register_craftitem("currency:minegeld_50", {
     description = S("50 Minegeld Note"),
     inventory_image = "minegeld_50.png",
+        stack_max = 30000,
+		groups = {minegeld = 1}
+})
+
+minetest.register_craftitem("currency:minegeld_100", {
+    description = S("100 Minegeld Note"),
+    inventory_image = "minegeld_100.png",
         stack_max = 30000,
 		groups = {minegeld = 1}
 })
