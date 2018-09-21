aa251997f351b268c0e0a18e7344e7f5fc8f0519
Vanessa Dannenberg <vanessa.e.dannenberg@gmail.com>
2018-09-21 17:08:02 -0400

    change references like "MineGeld" and "Mine-Geld" to "Minegeld" also minor doc fixes

--- craftitems.lua
+++ craftitems.lua
@@ -3,28 +3,28 @@ local MP = minetest.get_modpath(minetest.get_current_modname())
 local S, NS = dofile(MP.."/intllib.lua")
 
 minetest.register_craftitem("currency:minegeld", {
-    description = S("1 MineGeld Note"),
+    description = S("1 Minegeld Note"),
     inventory_image = "minegeld.png",
         stack_max = 30000,
 		groups = {minegeld = 1}
 })
 
 minetest.register_craftitem("currency:minegeld_5", {
-    description = S("5 MineGeld Note"),
+    description = S("5 Minegeld Note"),
     inventory_image = "minegeld_5.png",
         stack_max = 30000,
 		groups = {minegeld = 1}
 })
 
 minetest.register_craftitem("currency:minegeld_10", {
-    description = S("10 MineGeld Note"),
+    description = S("10 Minegeld Note"),
     inventory_image = "minegeld_10.png",
         stack_max = 30000,
 		groups = {minegeld = 1}
 })
 
 minetest.register_craftitem("currency:minegeld_50", {
-    description = S("50 MineGeld Note"),
+    description = S("50 Minegeld Note"),
     inventory_image = "minegeld_50.png",
         stack_max = 30000,
 		groups = {minegeld = 1}
