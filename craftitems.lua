11643571f2472fa8185b30f29e860cad36d1fc66
Vanessa Dannenberg <vanessa.e.dannenberg@gmail.com>
2018-09-21 18:19:36 -0400

    max out the stack size for notes

--- craftitems.lua
+++ craftitems.lua
@@ -26,40 +26,40 @@ minetest.register_craftitem("currency:minegeld_cent_25", {
 minetest.register_craftitem("currency:minegeld", {
     description = S("1 Minegeld Note"),
     inventory_image = "minegeld.png",
-        stack_max = 30000,
+        stack_max = 65535,
 		groups = {minegeld = 1}
 })
 
 minetest.register_craftitem("currency:minegeld_5", {
     description = S("5 Minegeld Note"),
     inventory_image = "minegeld_5.png",
-        stack_max = 30000,
+        stack_max = 65535,
 		groups = {minegeld = 1}
 })
 
 minetest.register_craftitem("currency:minegeld_10", {
     description = S("10 Minegeld Note"),
     inventory_image = "minegeld_10.png",
-        stack_max = 30000,
+        stack_max = 65535,
 		groups = {minegeld = 1}
 })
 
 minetest.register_craftitem("currency:minegeld_50", {
     description = S("50 Minegeld Note"),
     inventory_image = "minegeld_50.png",
-        stack_max = 30000,
+        stack_max = 65535,
 		groups = {minegeld = 1}
 })
 
 minetest.register_craftitem("currency:minegeld_100", {
     description = S("100 Minegeld Note"),
     inventory_image = "minegeld_100.png",
-        stack_max = 30000,
+        stack_max = 65535,
 		groups = {minegeld = 1}
 })
 
 minetest.register_craftitem("currency:minegeld_bundle", {
     description = S("Bundle of random Minegeld notes"),
     inventory_image = "minegeld_bundle.png",
-        stack_max = 30000,
+        stack_max = 65535,
 })
